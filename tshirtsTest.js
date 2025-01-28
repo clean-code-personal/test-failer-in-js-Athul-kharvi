0d9f7297f964c494e2886723a074c6f6fd128da3
Athul kharvi <98105204+Athul-kharvi@users.noreply.github.com>
2025-01-28 15:48:52 +0530

    Create tshirtsTest.js

---
+++ tshirtsTest.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+const { size } = require('./tshirts');
+
+describe('size function', () => {
+    it('should return S for sizes less than 38', () => {
+        expect(size(37)).to.equal('S');
+    });
+
+    it('should return S for size 38 (edge case)', () => {
+        expect(size(38)).to.equal('S');
+    });
+
+    it('should return M for sizes between 38 and 42', () => {
+        expect(size(40)).to.equal('M');
+    });
+
+    it('should return L for sizes greater than or equal to 42', () => {
+        expect(size(43)).to.equal('L');
+    });
+});
